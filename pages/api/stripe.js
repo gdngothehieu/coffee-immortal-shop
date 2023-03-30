5829a4121a6dc2679b803a4127d624b9a2a5e98c
tchong6@slb.com <ngothehieusan123@gmail.com>
2023-03-31 06:43:06 +0700

    snv

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,66 +1,17 @@
 import Stripe from "stripe";
-import * as paypal from "paypal-rest-sdk";
 
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
-paypal.configure({
-  mode: "live", //sandbox or live
-  client_id:
-    "AQCa2lmvPUd7Rf5EhrLkLX9f-k8CZgix-mMmayc9Gkpfg0MMr8y80Kxh1qeJBfSy55ZMI9BK7j9DBeI7",
-  client_secret:
-    "EOP0vtwqot4AWS1o84fPmgigoM-BIX1X1DPIvBpALRsaduDWWDMnyac42oWGuA3RvEFFNY9jlLAYLzMG",
-  openid_redirect_uri: "http://example.com",
-});
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      // const params = {
-      //   submit_type: "pay",
-      //   mode: "payment",
-      //   payment_method_types: ["card"],
-      //   billing_address_collection: "auto",
-      //   // shipping_options: [
-      //   //   { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
-      //   // ],
-      //   line_items: req.body.map((item) => {
-      //     const img = item.image[0].asset._ref;
-      //     const newImage = img
-      //       .replace(
-      //         "image-",
-      //         "https://cdn.sanity.io/images/vfxfwnaw/production/"
-      //       )
-      //       .replace("-webp", ".webp");
-
-      //     return {
-      //       price_data: {
-      //         currency: "vnd",
-      //         product_data: {
-      //           name: item.name,
-      //           images: [newImage],
-      //         },
-      //         unit_amount: item.price * 100,
-      //       },
-      //       adjustable_quantity: {
-      //         enabled: true,
-      //         minimum: 1,
-      //       },
-      //       quantity: item.quantity,
-      //     };
-      //   }),
-      //   success_url: `${req.headers.origin}/success`,
-      //   cancel_url: `${req.headers.origin}/canceled`,
-      // };
-
-      // Create Checkout Sessions from body params.
-      var create_payment_json = {
-        intent: "sale",
-        payer: {
-          payment_method: "paypal",
-        },
-        redirect_urls: {
-          return_url: `${req.headers.origin}/success`,
-          cancel_url: `${req.headers.origin}/canceled`,
-        },
-        transactions: req.body.map((item) => {
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [{ shipping_rate: "shr_1Kn3IaEnylLNWUqj5rqhg9oV" }],
+        line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img
             .replace(
@@ -68,37 +19,31 @@ export default async function handler(req, res) {
               "https://cdn.sanity.io/images/vfxfwnaw/production/"
             )
             .replace("-webp", ".webp");
+
           return {
-            item_list: {
-              items: [
-                {
-                  name: item.name,
-                  sku: "item",
-                  price: item.price * 100,
-                  currency: "VND",
-                  quantity: item.quantity,
-                },
-              ],
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
             },
-            amount: {
-              currency: "VND",
-              total: item.price * 100,
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
             },
-            description: "This is the Paypal.",
+            quantity: item.quantity,
           };
         }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
       };
 
-      paypal.payment.create(create_payment_json, function (error, payment) {
-        if (error) {
-          console.log(error);
-          throw error;
-        } else {
-          console.log("Create Payment Response");
-          console.log(payment);
-        }
-      });
-      res.status(200).json({ Success: "Success" });
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
